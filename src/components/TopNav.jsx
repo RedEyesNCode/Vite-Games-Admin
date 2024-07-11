5bfc1f9bf6ab7d8f72bcf2b5426c13f3e24b8638
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2024-07-11 10:16:16 +0530

    Update for flowbite ui and login admin form

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,9 +1,9 @@
+
+
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-const App = () => {
+const TopNav = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -48,4 +48,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default TopNav;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom";
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<App />} />
+      
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
